b9b7e7de05e68639f565e8e2fa9fd128d0a33795
Anand Upadhyay <anandgupadhyay03@gmail.com>
2023-10-28 11:28:58 +0530

    Initial Commit

---
+++ Learning/LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp.swift
+//  Learning
+//
+//  Created by Anand Upadhyay on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
