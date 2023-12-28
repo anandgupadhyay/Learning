eac616d8fdf078706590c29a1c0d42e4e5bdec20
Anand Upadhyay <anandgupadhyay03@gmail.com>
2023-12-28 14:00:41 +0530

    Create TypeAlias.swift
    TypeAlias Tutorial

---
+++ Learning/TypeAlias.swift
@@ -0,0 +1,18 @@
+/*    Swift Typealias is used to provide a new name for an existing data type in the program. Once you create a typealias, you can use the aliased name instead of the exsisting name throughout the program.
+ Typealias doesn't create a new data type, it simply provides a new name to the existing data type.
+
+ Purpose of using Typealias
+ The main purpose of using typealias is make our code clearer and human readable.
+
+ Create a typealias
+ Typealias is declared using the keyword typealias:
+ 
+
+ Typealias for Built-in Types
+ Typealias can be used for all built-in data types i.e. String, Int, Float etc.
+
+ For example:
+ typealias BusinessName = String
+ let name:BusinessName = "Simmy's Cacke Plaza"
+
+ */
