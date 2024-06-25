45c2dae09096ee5a80e8262d95c4e8dee82f5222
Anand Upadhyay <anandgupadhyay03@gmail.com>
2024-06-25 10:29:45 +0530

    Update PublisherSubscriber.swift
    Combine Example

--- Learning/PublisherSubscriber.swift
+++ Learning/PublisherSubscriber.swift
@@ -1,19 +1,30 @@
+import Foundation
 import Combine
-let myPublisher = PassthoughSubject<String, Never>O
-let mySubscriber = myPublisher.sink(
-receiveCompletion: { completion in
-switch
-completion {
-case
-•finished:
-print("Completed")
-case .failure(let error):
-print("Failed with error: \(error)")
+
+class CombineExample{
+    
+    let myPublisher = PassthroughSubject<String, Never>()
+    
+    func test1(){
+        let mySubscriber = myPublisher.sink(
+            receiveCompletion: { completion in
+                switch
+                completion {
+                case .finished:
+                    print("Completed")
+                case .failure(let error):
+                    print("Failed with error: \(error)")
+                }
+            },
+            receiveValue: { value in
+                print("Received value: \(value)")
+            }
+        )
+    }
+            
+    func test2(){
+        myPublisher.send("Hello")
+        myPublisher.send("World")
+        myPublisher.send(completion: .finished)
+    }
 }
-}.
-}
-receiveValue: { value in
-print("Received value: \(value)")
-myPublisher. send("Hello")
-myPublisher.send("World")
-myPublisher.send(completion: finished)
