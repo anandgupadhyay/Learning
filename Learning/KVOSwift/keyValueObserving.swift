827f070a6475f73c61e4dc36e038d31503d1df49
Anand Upadhyay <anandgupadhyay03@gmail.com>
2024-06-28 10:17:58 +0530

    Generate initialiser automatically

--- Learning/KVOSwift/keyValueObserving.swift
+++ Learning/KVOSwift/keyValueObserving.swift
@@ -5,6 +5,9 @@
 //  Created by Anand Upadhyay on 28/06/24.
 //
 
+//How to quickly generate initialiser for a class
+//Just create your class, add your properties and then Right Click on it > Refactor > Generate Memberwise initializer as such: This will automatically create an initializer for you, which is at least for me a huge time saver, and one of the reasons that always made structs so appealing for me
+
 import Foundation
 
 class KVOEmployee: NSObject{
