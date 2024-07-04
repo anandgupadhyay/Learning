a5ed61b9cb59000b1eb301c1c03668444d873f1d
Anand Upadhyay <anandgupadhyay03@gmail.com>
2024-07-04 10:27:17 +0530

    Added Background queue

--- Learning/CGD/GCDSwiftTest.swift
+++ Learning/CGD/GCDSwiftTest.swift
@@ -1,14 +1,24 @@
-let serialQ = DispatchQueue(label: "com.gcddemo.serialQ")
+//serial queue
+        let serialQ = DispatchQueue(label: "com.gcddemo.serialQ")
         serialQ.async {
             for i in 0..<5{
                 debugPrint("Index:\(i)")
             }
         }
-
-
-let concurrentQ = DispatchQueue(label: "com.gcddemo.serialQ",attributes: .concurrent)
+        
+        //concurrent queue
+        let concurrentQ = DispatchQueue(label: "com.gcddemo.serialQ",attributes: .concurrent)
         serialQ.async {
             for i in 0..<5{
                 debugPrint("Index:\(i)")
+            }
+        }
+        
+        //performing task in background
+        DispatchQueue.global(qos: .background).async {
+            //do some backgroud code like fetch data from server or process business logic
+            DispatchQueue.main.async {
+                //update UI
+                //this step is required to bring the exucution back to main thread
             }
         }
