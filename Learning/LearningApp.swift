7f67e4664b959dffb163439c9379acec9d556fc7
Anand Upadhyay <anandgupadhyay03@gmail.com>
2024-09-17 12:51:12 +0530

    Update LearningApp.swift
    Decimal formating

--- Learning/LearningApp.swift
+++ Learning/LearningApp.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+
+extension Double {
+    var clean: String {
+       return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+}
+
 @main
 struct LearningApp: App {
     var body: some Scene {
