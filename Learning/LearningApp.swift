cce5c0f51eea9f1b5304b07320dc7270ab46b7ee
Anand Upadhyay <anandgupadhyay03@gmail.com>
2024-09-17 12:51:23 +0530

    Update LearningApp.swift
    Added comment

--- Learning/LearningApp.swift
+++ Learning/LearningApp.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-
+//Extension to get string from double if the number has decimal points then it will display numbers if not then plain number
 extension Double {
     var clean: String {
        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
