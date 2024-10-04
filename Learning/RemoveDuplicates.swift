ba2e46c56326e3a58e3dabc3ea4e586e9cfa6365
Anand Upadhyay <anandgupadhyay03@gmail.com>
2024-10-04 11:16:40 +0530

    Update RemoveDuplicates.swift
    Updated remove duplicate code with example

--- Learning/RemoveDuplicates.swift
+++ Learning/RemoveDuplicates.swift
@@ -8,14 +8,46 @@ struct User: Codable {
 }
 
 
-func removeDuplicateUser(from users: [User]) -> [User] {
-    var uniqueUserIds: Set<String> = []
-    return users.filter { user in
-        if uniqueUserIds.contains(user.userId) {
-            return false
-        } else {
-            uniqueUserIds.insert(user.userId)
-            return true
+
+
+// Function to filter out duplicate users based on userId
+    func filterDuplicateUsers(from userList: [User]) -> [User] {
+        var seenUserIds = Set<String>()  // A set to track unique userIds
+        var filteredUsers = [User]()
+        
+        for user in userList {
+            if !seenUserIds.contains(user.userId) {
+                filteredUsers.append(user)
+                seenUserIds.insert(user.userId)
+            }
         }
+        
+        return filteredUsers
     }
-}
+
+    // Example usage
+    let users = [
+        User(userId: "user1"),
+        User(userId: "user2"),
+        User(userId: "user1"), // Duplicate user
+        User(userId: "user3"),
+        User(userId: "user2")  // Duplicate user
+    ]
+
+    let eligibleUsers = filterDuplicateUsers(from: users)
+
+    for user in eligibleUsers {
+        print(“\nUser eligible for discount: \(user.userName)")
+    }
+
+// func removeDuplicateUser(from users: [User]) -> [User] {
+//     var uniqueUserIds: Set<String> = []
+//     return users.filter { user in
+//         if uniqueUserIds.contains(user.userId) {
+//             return false
+//         } else {
+//             uniqueUserIds.insert(user.userId)
+//             return true
+//         }
+//     }
+// }
