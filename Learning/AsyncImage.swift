2590ca6452fb7a498a49a2fe03517f959cccac3d
Anand Upadhyay <anandgupadhyay03@gmail.com>
2024-11-19 11:25:08 +0530

    Error Handling in AsyncImage
    Error Handling in AsyncImage

--- Learning/AsyncImage.swift
+++ Learning/AsyncImage.swift
@@ -20,3 +20,18 @@ AsyncImage(url: URL(string: "https://picsum.photos/id/237/400/400")) { image in
 .border(Color.gray)
 
 
+//AsyncImage Error Handling
+
+AsyncImage(url: URL(string: "https://picsum.photos/i/237/400/400")) { phase in
+    if let image = phase.image {
+        image
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+    } else if phase.error != nil {
+        Text("No image available")
+    } else {
+        Image(systemName: "photo")
+    }
+}
+.frame(width: 250, height: 250)
+.border(Color.gray)
