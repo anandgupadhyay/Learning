155f44ace453de77f311927078a97e840077f3b5
Anand Upadhyay <anandgupadhyay03@gmail.com>
2024-12-04 10:28:25 +0530

    Create Extension+String.swift
    String Extension

---
+++ Learning/Extension+String.swift
@@ -0,0 +1,55 @@
+//
+//  Extension+String.swift
+//  eBoard
+//
+//  Created by Anand Upadhyay on 12/06/24.
+//
+
+import Foundation
+
+extension String {
+    func condenseWhitespace() -> String {
+        let components = components(separatedBy: .whitespacesAndNewlines)
+        return components.filter { !$0.isEmpty }.joined(separator: " ")
+      }
+    
+    enum TrimmingOptions {
+        case all
+        case leading
+        case trailing
+        case leadingAndTrailing
+    }
+    
+    func trimming(spaces: TrimmingOptions, using characterSet: CharacterSet = .whitespacesAndNewlines) ->  String {
+        switch spaces {
+        case .all: return trimmingAllSpaces(using: characterSet)
+        case .leading: return trimingLeadingSpaces(using: characterSet)
+        case .trailing: return trimingTrailingSpaces(using: characterSet)
+        case .leadingAndTrailing:  return trimmingLeadingAndTrailingSpaces(using: characterSet)
+        }
+    }
+    
+    private func trimingLeadingSpaces(using characterSet: CharacterSet) -> String {
+        guard let index = firstIndex(where: { !CharacterSet(charactersIn: String($0)).isSubset(of: characterSet) }) else {
+            return self
+        }
+
+        return String(self[index...])
+    }
+    
+    private func trimingTrailingSpaces(using characterSet: CharacterSet) -> String {
+        guard let index = lastIndex(where: { !CharacterSet(charactersIn: String($0)).isSubset(of: characterSet) }) else {
+            return self
+        }
+
+        return String(self[...index])
+    }
+    
+    private func trimmingLeadingAndTrailingSpaces(using characterSet: CharacterSet) -> String {
+        return trimmingCharacters(in: characterSet)
+    }
+    
+    private func trimmingAllSpaces(using characterSet: CharacterSet) -> String {
+        return components(separatedBy: characterSet).joined()
+    }
+}
