f8bef4a896b885ba72b124fc7d3e72d1d3ef3619
Anand Upadhyay <anandgupadhyay03@gmail.com>
2024-12-10 10:38:19 +0530

    Update GroupingMatching.swift
    How to use

--- Learning/OtherLearning/GroupingMatching.swift
+++ Learning/OtherLearning/GroupingMatching.swift
@@ -25,3 +25,13 @@ func groupRisks(risks: [Risk]) -> [[Risk]] {
 
     return groupedRisks
 }
+// Using
+let risks = [
+    Risk(name: "Risk One", probability: 5, impact: 4, id: 1),
+    Risk(name: "Risk Two", probability: 4, impact: 2, id: 2),
+    Risk(name: "Risk Three", probability: 4, impact: 2, id: 3),
+    Risk(name: "Risk Four", probability: 5, impact: 4, id: 4)
+]
+
+let groupedRisks = groupRisks(risks: risks)
+print(groupedRisks)
