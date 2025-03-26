c37cd10ab3f8da845b0224c70a397bcfbcfc4590
Anand Upadhyay <anandgupadhyay03@gmail.com>
2025-03-26 10:24:04 +0530

    Create DIP.swift
    DIP of SOLID

---
+++ Learning/SOLID/DIP.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+//Step 1: Define the Protocol
+
+protocol Notification {
+    func send(message: String)
+}
+
+//Step 2: Create Concrete Implementations
+
+class EmailNotification: Notification {
+    func send(message: String) {
+        print("Sending Email with message: \(message)")
+    }
+}
+
+class SMSNotification: Notification {
+    func send(message: String) {
+        print("Sending SMS with message: \(message)")
+    }
+}
+
+//Step 3: Create the Notification Service
+class NotificationService {
+    private let notification: Notification
+    
+    // Dependency Injection through initializer
+    init(notification: Notification) {
+        self.notification = notification
+    }
+    
+    func notify(message: String) {
+        notification.send(message: message)
+    }
+}
+
