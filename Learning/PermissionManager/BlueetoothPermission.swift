a35eb87d2e79d6511083392fb086e7ab0795d217
Anand Upadhyay <anandgupadhyay03@gmail.com>
2025-06-27 11:23:41 +0530

    Create BlueetoothPermission.swift
    Blue tooth Permission manager

---
+++ Learning/PermissionManager/BlueetoothPermission.swift
@@ -0,0 +1,34 @@
+Swift Implementation (Example):
+
+Code
+
+import CoreBluetooth
+
+class ViewController: UIViewController, CBCentralManagerDelegate {
+    var centralManager: CBCentralManager!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            // Start scanning for peripherals
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+        }
+    }
+
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        // Handle discovered peripheral
+    }
+
+    // Implement state restoration methods (optional but recommended)
+    func application(_ application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool {
+        return true
+    }
+
+    func application(_ application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
+        return true
+    }
+}
