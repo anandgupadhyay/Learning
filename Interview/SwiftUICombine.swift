9812f0f1717eb2924521d88d1df5e3c9d4256a20
Anand Upadhyay <anandgupadhyay03@gmail.com>
2025-09-04 11:18:52 +0530

    Create SwiftUICombine.swift
    SwiftUI and Combine Interview questions

---
+++ Interview/SwiftUICombine.swift
@@ -0,0 +1,25 @@
+Here are some of the key questions that came up:
+
+1. What are PassThroughSubject and CurrentValueSubject?
+2. Explain Publisher and Subscriber.
+3. Difference between @State and @Binding in SwiftUI.
+4. What is AnyPublisher?
+5. When do we use eraseToAnyPublisher?
+6. What is AnyView?
+7. What does the @Published property wrapper do?
+8. Write a Combine pipeline to fetch data from two different network requests and output only when both complete. (💻 Live Coding Test)
+9. What is a Cancellable?
+10.Types of operators in Combine.
+11. Usage of sink with completion.
+12. Explain switchToLatest.
+13. Build a 2-screen SwiftUI app (list + detail) that makes an API call using Combine only. (⏱️ 20-min Live Coding Test)
+14. Difference between onReceive and onChange modifiers.
+15. Coding Exercise:
+👉 Given an array of strings, transform it into a new array:
+Input → ["a", "b", "c"]
+Output → ["c-0", "b-1", "a-2"]
+⚡️ Requirement: Solve this using Swift higher-order functions such as map, enumerated, etc.
+
+💡 If you’re preparing for iOS interviews, these are excellent questions to sharpen your fundamentals and practical skills in SwiftUI + Combine.
+
+🤝 What other Swift / SwiftUI interview questions have you faced recently? Let’s share and learn together!
