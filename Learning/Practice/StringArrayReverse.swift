7fb29e0859368d58d4c09c756e1434b5cc2c5d08
Anand Upadhyay <anandgupadhyay03@gmail.com>
2025-09-17 11:50:47 +0530

    Update StringArrayReverse.swift
    Create a Dictionary of Different Types and its respective item array

--- Learning/Practice/StringArrayReverse.swift
+++ Learning/Practice/StringArrayReverse.swift
@@ -6,3 +6,31 @@
 
 let array = ["a", "b", "c"]
 let resultArray = array.enumerated().map { "\($0.element)-\($0.offset)" }
+
+
+//What will be output 
+
+let array: [Any] = ["string", 12, 23.45, true, Date(), ["name": "anand"], "string2", 54.54, 6, false, Date()]
+
+var typeDictionary: [String: [Any]] = [:]
+
+for item in array {
+    switch item {
+    case is String:
+        typeDictionary["String", default: []].append(item)
+    case is Int:
+        typeDictionary["Int", default: []].append(item)
+    case is Double:
+        typeDictionary["Double", default: []].append(item)
+    case is Bool:
+        typeDictionary["Bool", default: []].append(item)
+    case is Date:
+        typeDictionary["Date", default: []].append(item)
+    case is [String:Any]:
+        typeDictionary["Dictionary", default: []].append(item)
+    default:
+        typeDictionary["Other", default: []].append(item)
+    }
+}
+
+print(typeDictionary)
