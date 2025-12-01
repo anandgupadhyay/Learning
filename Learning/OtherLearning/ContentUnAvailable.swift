fd825f2784665339829e70d3b183996b75b061d0
Anand Upadhyay <anandgupadhyay03@gmail.com>
2025-12-01 10:35:06 +0530

    Update ContentUnAvailable.swift

--- Learning/OtherLearning/ContentUnAvailable.swift
+++ Learning/OtherLearning/ContentUnAvailable.swift
@@ -9,3 +9,22 @@ struct EmptyStateView: View {
         )
     }
 }
+
+//Using it inside a list or screen
+struct MyListView: View {
+    @State private var items: [String] = []
+
+    var body: some View {
+        if items.isEmpty {
+            ContentUnavailableView(
+                "No Items",
+                systemImage: "list.bullet",
+                description: Text("Add new items to see them here.")
+            )
+        } else {
+            List(items, id: \.self) { item in
+                Text(item)
+            }
+        }
+    }
+}
