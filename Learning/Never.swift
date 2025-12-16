714a1fa6c6debcd9f546c04e42af02d1edb634ab
Anand Upadhyay <anandgupadhyay03@gmail.com>
2025-12-16 10:13:32 +0530

    Never in Swift
    Using Never in Swift

---
+++ Learning/Never.swift
@@ -0,0 +1,17 @@
+/*
+What is Never in Swift?
+
+Never is a special type that means:
+
+“This code path can never happen.”
+
+A function returning Never:
+
+Never returns normally
+
+Always crashes, exits, or loops forever
+
+A generic using Never:
+
+Indicates impossibility (e.g. a Result that cannot fail or cannot succeed)
+*/
